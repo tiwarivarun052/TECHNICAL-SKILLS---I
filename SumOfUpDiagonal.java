abd4bf27e7c4814f5d217eb335506bb5624ac03d
VARUN TIWARI <vishaltiwari6398810969@gmail.com>
2026-02-10 11:47:28 +0530

    commit

---
+++ SumOfUpDiagonal.java
@@ -0,0 +1,28 @@
+// Q: WAP sum of upper diagonal elements of a matrix.
+
+import java.util.Scanner;
+public class SumOfUpDiagonal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no. of rows: ");
+        int r = sc.nextInt();
+        System.out.print("Enter no. of cols: ");
+        int c = sc.nextInt();
+        int[][] matrix = new int[r][c];
+        System.out.print("Enter elements: ");
+        for (int i=0; i<r; i++) {
+            for (int j=0; j<c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        int sum = 0;
+        for (int i=0; i<r; i++) {
+            for (int j=0; j<c; j++) {
+                if (j>i) {
+                    sum+=matrix[i][j];
+                }
+            }
+        }
+        System.out.print("Sum: "+sum);
+    }
+}
